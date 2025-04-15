3712f80e3149fa51070dc51659b261812c4410a5
CaioMiranda12 <caioclm04@gmail.com>
2025-04-15 19:37:19 -0300

    feat: tabela de user atualizada e adicionada a rota de createUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,7 +13,7 @@ export const findOneUser = async (req, res) => {
   if (!id) {
     return res
       .status(StatusCodes.NOT_FOUND)
-      .json({ message: 'ID de usuário não encontrado' });
+      .json({ error: 'ID de usuário não encontrado' });
   }
 
   try {
@@ -27,6 +27,38 @@ export const findOneUser = async (req, res) => {
   } catch (error) {
     return res
       .status(StatusCodes.NOT_FOUND)
-      .json({ message: 'Usuário não encontrado' });
+      .json({ error: 'Usuário não encontrado' });
+  }
+};
+
+export const createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExits = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (userExits) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: 'E-mail já cadastrado' });
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash: password,
+      },
+    });
+
+    return res.status(StatusCodes.CREATED).json(user);
+  } catch (error) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: 'Falha ao criar usuário' });
   }
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { findAllUsers, findOneUser } from '../controllers/user.controller';
+import {
+  createUser,
+  findAllUsers,
+  findOneUser,
+} from '../controllers/user.controller';
 
 const routes = new Router();
 
 routes.get('/', findAllUsers);
 routes.get('/:id', findOneUser);
+routes.post('/', createUser);
 
 export default routes;
